ff1ef79ee9905f8c7d9d9318a4fb79449505963e
egijm <90076976+ramdhey@users.noreply.github.com>
2024-09-16 00:38:47 +0700

    Pesan commit Anda

---
+++ database/migrations/2024_09_07_044631_create_pasiens_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+         public function up(): void
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_pasien');
+            $table->string('nama');
+            $table->string('umur');
+            $table->string('foto')->nullable();
+            $table->string('jenis_kelamin');
+            $table->string('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pasiens');
+    }
+};
